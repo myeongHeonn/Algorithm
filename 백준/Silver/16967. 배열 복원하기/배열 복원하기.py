1af94899a72f75881da5863be75e5578f9847c73
Myeongheon Oh <axm0219@naver.com>
2024-09-13 13:06:09 +0900

    [Silver III] Title: 배열 복원하기, Time: 120 ms, Memory: 38120 KB -BaekjoonHub

--- 백준/Silver/16967. 배열 복원하기/배열 복원하기.py
+++ 백준/Silver/16967. 배열 복원하기/배열 복원하기.py
@@ -7,24 +7,15 @@ arrayB = []
 for _ in range(h+x):
     arrayB.append(list(map(int, input().split())))
 
-arrayA = [[False] * w for _ in range(h)]
+arrayA = [[0] * w for _ in range(h)]
 
-for i in range(h+x):
-    for j in range(w+y):
-        # 겹치는 범위가 아니면
-        if i < x or i > h-x or j < y or j > w-y:
-            if (h <= i and j <= y-1) or (w <= j and i <= x-1) :
-                continue
-            if 0 <= i <= h-1 and 0 <= j <= w-1:
-                arrayA[i][j] = arrayB[i][j]
-            else:
-                arrayA[i-x][j-y] = arrayB[i][j]
+for i in range(h):
+    for j in range(w):
+        arrayA[i][j] = arrayB[i][j]
 
-for i in range(h+x):
-    for j in range(w+y):
-        if x <= i <= h-x and y <= j <= w-y:
-            if not arrayA[i][j]:
-                arrayA[i][j] = arrayB[i][j] - arrayA[i-x][j-y]
+for i in range(x,h):
+    for j in range(y,w):
+        arrayA[i][j] = arrayB[i][j] - arrayA[i-x][j-y]
 
 for row in arrayA:
     print(*row)
