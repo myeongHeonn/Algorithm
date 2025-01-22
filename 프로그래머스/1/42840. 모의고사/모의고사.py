385a5a272d75c45af9cd8402e16982253d4b3f26
Myeongheon Oh <axm0219@naver.com>
2025-01-22 13:22:14 +0900

    [level 1] Title: 모의고사, Time: 5.17 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,24 +1,20 @@
 def solution(answers):
-    answer = []
-    m1 = [1,2,3,4,5]
-    m2 = [2,1,2,3,2,4,2,5]
-    m3 = [3,3,1,1,2,2,4,4,5,5]
+    patterns = [[1,2,3,4,5],
+                [2,1,2,3,2,4,2,5],
+                [3,3,1,1,2,2,4,4,5,5]]
+    
+    scores = [0] * 3
+    
+    for i, answer in enumerate(answers):
+        for j, pattern in enumerate(patterns):
+            if answer == pattern[i%len(pattern)]:
+                scores[j] += 1
+                
+    max_score = max(scores)
+    result = []
 
-    cnt1, cnt2, cnt3 = 0, 0, 0
-    for idx in range(len(answers)):
-        if answers[idx] == m1[idx%5]:
-            cnt1 += 1
-        if answers[idx] == m2[idx%8]:
-            cnt2 += 1
-        if answers[idx] == m3[idx%10]:
-            cnt3 += 1
-
-    max_cnt = max(cnt1, cnt2, cnt3)
-    if max_cnt == cnt1:
-        answer.append(1)
-    if max_cnt == cnt2:
-        answer.append(2)
-    if max_cnt == cnt3:
-        answer.append(3)
-
-    return answer
+    for i, score in enumerate(scores):
+        if score == max_score:
+            result.append(i+1)
+    
+    return result
