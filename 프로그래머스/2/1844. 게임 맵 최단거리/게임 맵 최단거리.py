33c398f3a08137699bd082d668230daa4d6db51e
Myeongheon Oh <axm0219@naver.com>
2025-03-09 21:49:28 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 13.18 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,39 +1,35 @@
 from collections import deque
 
 def solution(maps):
-    answer = -1
+    dy = [-1, 1, 0, 0]
+    dx = [0, 0, -1, 1]
 
     n = len(maps)
     m = len(maps[0])
 
-    visited = [[-1 for _ in range(m)] for _ in range(n)]
-
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
+    dist = [[-1] * m for _ in range(n)]
 
     queue = deque()
-    queue.append((0,0))
-    visited[0][0] = 1
+    queue.append((0, 0))
+    dist[0][0] = 1
 
     while queue:
-        x, y = queue.popleft()
+        y, x = queue.popleft()
 
-        if x == n-1 and y == m-1:
-            answer = visited[x][y]
-            break
+        if y == n-1 and x == m-1:
+            return dist[y][x]
 
         for i in range(4):
-            nx = x + dx[i]
             ny = y + dy[i]
+            nx = x + dx[i]
 
-            if nx < 0 or nx >=n or ny < 0 or ny >= m:
+            if ny < 0 or ny >= n or nx < 0 or nx >= m:
                 continue
-            if visited[nx][ny] != -1:
-                continue
-            if maps[nx][ny] == 0:
+            if  maps[ny][nx] == 0:
                 continue
 
-            visited[nx][ny] = visited[x][y] + 1
-            queue.append((nx, ny))
+            if dist[ny][nx] == -1:
+                queue.append((ny, nx))
+                dist[ny][nx] = dist[y][x] + 1
 
-    return answer
+    return -1
