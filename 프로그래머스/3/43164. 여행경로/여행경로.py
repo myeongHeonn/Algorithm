f59b75859183f4237434bc272b8b0c09e437deba
Myeongheon Oh <axm0219@naver.com>
2025-03-12 12:40:50 +0900

    [level 3] Title: 여행경로, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,21 +1,21 @@
 from collections import defaultdict
 
+def dfs(node, graph, path):
+    while graph[node]:
+        next_node = graph[node].pop()
+        dfs(next_node, graph, path)
+    path.append(node)
+
 def solution(tickets):
     graph = defaultdict(list)
-    
+
     for parent, child in tickets:
         graph[parent].append(child)
-        
+
     for key in graph:
         graph[key].sort(reverse=True)
-        
+
     path = []
-    
-    def dfs(node):
-        while graph[node]:
-            next_node = graph[node].pop()
-            dfs(next_node)
-        path.append(node)
-        
-    dfs("ICN")
+
+    dfs("ICN", graph, path)
     return path[::-1]
