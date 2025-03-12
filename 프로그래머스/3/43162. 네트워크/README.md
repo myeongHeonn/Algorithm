35a319f9e840d31cd90d1d4c80860c9842d3a91e
Myeongheon Oh <axm0219@naver.com>
2025-03-12 11:19:29 +0900

    [level 3] Title: 네트워크, Time: 0.51 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.30 ms
+메모리: 10.4 MB, 시간: 0.51 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 30일 16:56:21
+2025년 03월 12일 11:19:26
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,18 +1,17 @@
-def solution(n, computers):
-    visited = [False for _ in range(n)]
-    answer = 0
+def dfs(computers, visited, node):
+    visited[node] = True
+    for idx, connected in enumerate(computers[node]):
+        if connected and not visited[idx]:
+            dfs(computers, visited, idx)
 
-    for computer in range(n):
-        if not visited[computer]:
-            dfs(n, computers, computer, visited)
-            # dfs로 컴퓨터들을 최대한 방문 -> 연결된 하나의 네트워크
-            answer += 1
+def solution(n, computers):
+    answer = 0
+    visited = [False for _ in range(n)]
+
+    for i in range(n):
+        if visited[i]:
+            continue
+        dfs(computers, visited, i)
+        answer += 1
 
     return answer
-
-def dfs(n, computers, computer, visited):
-    visited[computer] = True
-    for con in range(n):
-        # 컴퓨터가 연결되어 있으면 dfs
-        if not visited[con] and computers[computer][con]:
-            dfs(n, computers, con, visited)
