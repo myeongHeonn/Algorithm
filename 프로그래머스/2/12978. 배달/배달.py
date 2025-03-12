1b9222b2abe0e1a49bcda69f3c4b6aa027659367
Myeongheon Oh <axm0219@naver.com>
2025-03-12 14:22:13 +0900

    [level 2] Title: 배달, Time: 2.45 ms, Memory: 9.52 MB -BaekjoonHub

---
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -0,0 +1,28 @@
+import heapq
+
+def solution(N, road, K):
+    graph = [[] for _ in range(N + 1)]
+    distances = [float("inf")] * (N + 1)
+    distances[1] = 0
+    
+    for a, b, cost in road:
+        graph[a].append((b, cost))
+        graph[b].append((a, cost))
+        
+    heap = []
+    heapq.heappush(heap, (0, 1))
+    while heap:
+        dist, node = heapq.heappop(heap)
+        
+        for next_node, next_dist in graph[node]:
+            cost = dist + next_dist
+            if cost < distances[next_node]:
+                distances[next_node] = cost
+                heapq.heappush(heap, (cost, next_node))
+                
+    answer = 0
+    for dist in distances:
+        if dist <= K:
+            answer += 1
+    
+    return answer
