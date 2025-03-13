ae0dfcd63519c6bb8e1684e7b5d999c08bfb201e
Myeongheon Oh <axm0219@naver.com>
2025-03-13 12:30:28 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.35 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,47 +1,27 @@
-from collections import defaultdict
-import sys
-
-def dfs(node, graph, visited):
-    visited[node] = True
-    for next_node in graph[node]:
-        if visited[next_node]:
-            continue
-        dfs(next_node, graph, visited)
-
-def count_true(visited):
-    count = 0
-    for flag in visited:
-        if flag:
-            count += 1
-    return count
-
 def solution(n, wires):
-    answer = sys.maxsize
-
-    for i in range(len(wires)):
-        graph = defaultdict(list)
-        for idx, (a, b) in enumerate(wires):
-            # 전력망을 하나 자름
-            if i == idx:
-                continue
-            graph[a].append(b)
-            graph[b].append(a)
-
-        visited = [True] + [False for _ in range(n)]
-        count = 0
-        net = []
-
-        for key in range(1, len(wires) + 1):
-            if visited[key]:
-                continue
-            dfs(key, graph, visited)
-            count += 1
-            net.append(count_true(visited))
-
-            if count == 2:
-                if all(visited):
-                    answer = min(answer, abs((net[1] - net[0]) - (net[0] - 1)))
-                    break
-                else:
-                    break
+    graph = [[] for _ in range(n+1)]
+    for a, b in wires:
+        graph[a].append(b)
+        graph[b].append(a)
+        
+    def dfs(node, parent):
+        cnt = 1
+        for child in graph[node]:
+            if child != parent:
+                cnt += dfs(child, node)
+        return cnt
+    
+    answer = float("inf")
+    for a, b in wires:
+        graph[a].remove(b)
+        graph[b].remove(a)
+        
+        count_a = dfs(a, b)
+        count_b = n - count_a
+        
+        answer = min(answer, abs(count_a - count_b))
+        
+        graph[a].append(b)
+        graph[b].append(a)
+        
     return answer
