a6a7c2db9e5a4477a055aa06b6dca1ec470711f5
Myeongheon Oh <axm0219@naver.com>
2025-03-28 17:01:37 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.03 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.py
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.py
@@ -1,14 +1,2 @@
-from collections import defaultdict
-
 def solution(strings, n):
-    answer = []
-    dic = defaultdict(list)
-
-    for string in strings:
-        dic[string[n]].append(string)
-
-    for i in range(26):
-        if chr(i + 97) in dic:
-            dic[chr(i + 97)].sort()
-            answer += dic[chr(i + 97)]
-    return answer
+    return sorted(strings, key=lambda x: (x[n], x))
