bb5b5dda6cd7baac468f67dd8749f92cd5671442
Myeongheon Oh <axm0219@naver.com>
2025-04-01 16:39:03 +0900

    [level 2] Title: H-Index, Time: 68.15 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -2,7 +2,7 @@ def solution(citations):
     h = 0
     n = len(citations)
     
-    for i in range(n + 1):
+    for i in range(1, n + 1):
         count = 0
         for citation in citations:
             if citation >= i:
