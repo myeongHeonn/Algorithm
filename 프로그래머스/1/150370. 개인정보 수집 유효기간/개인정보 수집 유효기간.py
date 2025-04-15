51038e990fe0abe5092b695bc660f49aaab4ef43
Myeongheon Oh <axm0219@naver.com>
2025-04-15 18:37:43 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.46 ms, Memory: 9.44 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,37 +1,38 @@
 def solution(today, terms, privacies):
     answer = []
-    # 오늘 날짜를 년, 월, 일로 나눠서 리스트에 담아줌
-    today_list = list(map(int, today.split('.'))) # [2022, 5, 19]
-    # 약관 종류와 유효기간을 딕셔너리에 담아줌
-    terms_dict = {}
+    dic_terms = {}
     for term in terms:
-        temp_term = term.split()
-        terms_dict[temp_term[0]] = int(temp_term[1]) # {'A':'6'}
-    num = 0 # 번호
-    for privacy in privacies:
-        num += 1
-        temp = privacy.split()
-        temp_date = list(map(int, temp[0].split('.'))) # [2021, 5, 2]
-        temp_sort = temp[1] # A
-        # 유효기간
-        temp_date[1] += terms_dict[temp_sort]
-        if temp_date[1] > 12:
-            if temp_date[1] % 12 == 0:
-                temp_date[0] += (temp_date[1]//12 -1)
-                temp_date[1] = 12
-            else:
-                temp_date[0] += (temp_date[1] // 12)
-                temp_date[1] = temp_date[1] % 12
-        # 유효기간 지났는지 비교
-        if today_list[0] > temp_date[0]:
-            answer.append(num)
+        dic_terms[term[0]] = term[2:]
+
+    today_year = int(today[:4])
+    today_month = int(today[5:7])
+    today_day = int(today[8:10])
+
+    for idx, privacy in enumerate(privacies):
+        col_day = privacy[:10]
+        col_alpha = privacy[-1]
+        col_term = int(dic_terms[col_alpha])
+
+        col_year = int(col_day[:4])
+        col_month = int(col_day[5:7])
+        col_day = int(col_day[8:10])
+
+        cmp_month = (col_month + col_term) % 12
+        cmp_month = 12 if cmp_month == 0 else cmp_month
+        cmp_year = col_year + ((col_month + col_term - 1) // 12)
+        cmp_day = col_day
+
+        if today_year > cmp_year:
+            answer.append(idx + 1)
             continue
-        elif today_list[0] == temp_date[0]:
-            if today_list[1] > temp_date[1]:
-                answer.append(num)
+        elif today_year == cmp_year:
+            if today_month > cmp_month:
+                answer.append(idx + 1)
                 continue
-            elif today_list[1] == temp_date[1]:
-                if today_list[2] >= temp_date[2]:
-                    answer.append(num)
+            elif today_month == cmp_month:
+                if today_day >= cmp_day:
+                    answer.append(idx + 1)
+                    continue
+        print(answer)
 
     return answer
