d472c4574c73be5cfe1b32310c512baef10b1bea
Myeongheon Oh <axm0219@naver.com>
2025-04-17 08:57:39 +0900

    [level 3] Title: 단속카메라, Time: 0.88 ms, Memory: 9.66 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,11 +1,11 @@
 def solution(routes):
     answer = 0
+    routes.sort(key=lambda x:x[1])
     camera = -30001
-    routes.sort(key=lambda x: x[1])
     
     for start, end in routes:
         if start > camera:
             camera = end
             answer += 1
-
+    
     return answer
