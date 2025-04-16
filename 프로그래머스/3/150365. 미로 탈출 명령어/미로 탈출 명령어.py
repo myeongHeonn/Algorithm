ef1ea644f835973f0d0441c10a9aa3595f633758
Myeongheon Oh <axm0219@naver.com>
2025-04-16 11:08:26 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 14.42 ms, Memory: 15.3 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.py
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.py
@@ -1,32 +1,35 @@
 import sys
 sys.setrecursionlimit(3000)
 
+def is_move(n, m, x, y):
+    return 1 <= x <= n and 1 <= y <= m
+
+def can_reach(x, y, r, c, left_distance):
+    distance = abs(x - r) + abs(y - c)
+    return distance <= left_distance and (left_distance - distance) % 2 == 0
+
+
 def solution(n, m, x, y, r, c, k):
     answer = []
-
     directions = [('d', 1, 0), ('l', 0, -1), ('r', 0, 1), ('u', -1, 0)]
 
-    def can_reach(cx, cy, steps_left):
-        dist = abs(cx - r) + abs(cy - c)
-        return dist <= steps_left and (steps_left - dist) % 2 == 0
-
-    def dfs(cx, cy, path, steps_left):
+    def dfs(cx, cy, path, left_distance):
         if answer:
-            return  # 이미 정답 찾음 (사전순 가장 빠른 경로)
-        if steps_left == 0:
-            if (cx, cy) == (r, c):
+            return
+        
+        if left_distance == 0:
+            if cx == r and cy == c:
                 answer.append(path)
             return
 
         for d, dx, dy in directions:
             nx, ny = cx + dx, cy + dy
-            if 1 <= nx <= n and 1 <= ny <= m:
-                if can_reach(nx, ny, steps_left - 1):
-                    dfs(nx, ny, path + d, steps_left - 1)
-
-    if not can_reach(x, y, k):
+            if is_move(n, m, nx, ny) and can_reach(nx, ny, r, c, left_distance - 1):
+                dfs(nx, ny, path + d, left_distance - 1)
+                
+    if not can_reach(x, y, r, c, k):
         return "impossible"
 
-    dfs(x, y, "", k)
+    dfs(x, y, '', k)
 
-    return answer[0] if answer else "impossible"
+    return answer[0] if answer else 'impossible'
