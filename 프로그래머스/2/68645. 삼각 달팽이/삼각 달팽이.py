fd5fc07eae16b12871e9b655497ea93af18333e4
Myeongheon Oh <axm0219@naver.com>
2025-04-29 09:36:02 +0900

    [level 2] Title: 삼각 달팽이, Time: 154.77 ms, Memory: 42.4 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
@@ -1,25 +1,22 @@
 def solution(n):
+    answer = []
     triangle = [[0] * (i + 1) for i in range(n)]
     dx = [1, 0, -1]
     dy = [0, 1, -1]
-    
-    x, y = 0, 0
     direction = 0
-    
-    for num in range(1, n * (n + 1) // 2 + 1):
-        triangle[x][y] = num
-        
-        nx = x + dx[direction]
-        ny = y + dy[direction]
-        
+    x, y = 0, 0
+
+    for i in range(1, (n * (n + 1)) // 2 + 1):
+        triangle[x][y] = i
+
+        nx, ny = x + dx[direction], y + dy[direction]
         if 0 <= nx < n and 0 <= ny <= nx and triangle[nx][ny] == 0:
             x, y = nx, ny
         else:
             direction = (direction + 1) % 3
-            x += dx[direction]
-            y += dy[direction]
-            
-    answer = []
+            x = x + dx[direction]
+            y = y + dy[direction]
+
     for row in triangle:
         answer += row
     return answer
