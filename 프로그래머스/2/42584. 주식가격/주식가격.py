8269642b5e0fe5095da4f07b444e1623eec36d4a
Myeongheon Oh <axm0219@naver.com>
2025-05-13 16:03:25 +0900

    [level 2] Title: 주식가격, Time: 40.71 ms, Memory: 18.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,17 +1,16 @@
 def solution(prices):
-    n = len(prices)
-    answer = [0 for _ in range(n)]
+    answer = [0] * len(prices)
+    stack = []
 
-    # 스택을 사용해 이전 가격과 현재 가격 비교
-    stack = [0]
-    for i in range(1, n):
-        while stack and prices[i] < prices[stack[-1]]:
-            # 가격이 떨어졌을경우 이전 가격의 기간 계산
-            j = stack.pop()
-            answer[j] = i - j
-        stack.append(i)
-    # 스택에 남아 있는 가격들은 가격이 떨어지지 않은 경우
-    while stack:
-        j = stack.pop()
-        answer[j] = n - 1 - j
+    for idx, price in enumerate(prices):
+        while stack and price < stack[-1][1]:
+            answer[stack[-1][0]] = idx - stack[-1][0]
+            stack.pop()
+
+        stack.append((idx, price))
+
+    for i in range(len(answer) - 1):
+        if answer[i] == 0:
+            answer[i] = len(answer) - 1 - i
+
     return answer
