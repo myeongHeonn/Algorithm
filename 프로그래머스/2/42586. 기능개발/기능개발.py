d8d086f635a0f55a105c08d4d200e4614049d1b2
Myeongheon Oh <axm0219@naver.com>
2025-05-13 13:43:23 +0900

    [level 2] Title: 기능개발, Time: 0.88 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,24 +1,21 @@
-from collections import deque
-
 def solution(progresses, speeds):
     answer = []
-    q_progresses = deque(progresses)
-    q_speeds = deque(speeds)
-
-    while q_progresses:
-        # 작업 진행
-        for i in range(len(q_progresses)):
-            q_progresses[i] += q_speeds[i]
-
-        # 배포 가능 여부 확인
-        cnt = 0
-        # 맨 앞이 100 이상이면 배포
-        while q_progresses and q_progresses[0] >= 100:
-            q_progresses.popleft()
-            q_speeds.popleft()
-            cnt += 1
-
-        if cnt > 0:
-            answer.append(cnt)
+    n = len(progresses)
+    
+    index = 0    
+    while True:
+        if index == n:
+            break
+        
+        for i in range(n):
+            progresses[i] += speeds[i]
+            
+        count = 0
+        while index < n and progresses[index] >= 100:
+            count += 1
+            index += 1
+            
+        if count != 0:
+            answer.append(count)
 
     return answer
