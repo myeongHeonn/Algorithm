8179e3777f9e4fd75979a26bb7d2d065a71479d3
Myeongheon Oh <axm0219@naver.com>
2025-05-13 15:16:59 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 66.55 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,29 +1,23 @@
 from collections import deque
 
 def solution(bridge_length, weight, truck_weights):
-    truck_weights = deque(truck_weights)
-    # 다리 위에 있는 트럭 위치 (트럭의 무게, 위치)
-    truck_position = deque()
-    # 다리의 현재 무게
-    bridge_weight = 0
-    # 시간
-    time = 0
+    bridge = deque()
+    trucks = deque(truck_weights)
 
-    while truck_position or truck_weights:
+    bridge_weight = 0
+    time = 0
+    while trucks or bridge:
         time += 1
-        # 다리 위에 있는 트럭 위치 업데이트
-        if truck_position:
-            for truck in truck_position:
-                truck[1] += 1
-        # 다리 위에 있는 트럭 중 위치가 끝에 도달하면 팝
-        if truck_position and truck_position[0][1] == bridge_length + 1:
-            bridge_weight -= truck_position[0][0]
-            truck_position.popleft()
-        # 다리에 올라갈 수 있는 트럭 수 제한
-        if len(truck_position) >= bridge_length:
-            continue
-        if truck_weights and truck_weights[0] + bridge_weight <= weight:
-            bridge_weight += truck_weights[0]
-            truck_position.append([truck_weights.popleft(), 1])
+
+        if bridge:
+            if time - bridge[0][1] >= bridge_length:
+                bridge_weight -= bridge[0][0]
+                bridge.popleft()
+
+        if trucks and len(bridge) < bridge_length:
+            if bridge_weight + trucks[0] <= weight:
+                truck_weight = trucks.popleft()
+                bridge.append((truck_weight, time))
+                bridge_weight += truck_weight
 
     return time
