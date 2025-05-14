0af933adb70446f09a634d0914d90ddb20150979
Myeongheon Oh <axm0219@naver.com>
2025-05-14 10:22:07 +0900

    [level 1] Title: K번째수, Time: 0.02 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -2,5 +2,6 @@ def solution(array, commands):
     answer = []
     for command in commands:
         i, j, k = command[0], command[1], command[2]
-        answer.append(sorted(array[i-1:j])[k-1])
+        arr = sorted(array[i - 1:j])
+        answer.append(arr[k - 1])
     return answer
