34f6a8851733b3ed68e5b5d18102f1c7d1894435
Myeongheon Oh <axm0219@naver.com>
2025-05-21 10:02:32 +0900

    [level 2] Title: 카펫, Time: 0.09 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,13 +1,21 @@
 def solution(brown, yellow):
     answer = []
-    k = 0
-
+    k = 1
     while True:
-        k += 1
-        if yellow % k == 0:
-            if (k * 2) + ((yellow // k) * 2 + 4) == brown:
-                answer.append(yellow // k + 2)
-                answer.append(k + 2)
-                break
-
+        yellow_h = k
+        if yellow % k != 0:
+            k += 1
+            continue
+        yellow_w = yellow // k
+        
+        h = yellow_h + 2
+        w = yellow_w + 2
+        
+        if h * w == brown + yellow:
+            answer.append(w)
+            answer.append(h)
+            break
+        else:
+            k += 1
+    
     return answer
