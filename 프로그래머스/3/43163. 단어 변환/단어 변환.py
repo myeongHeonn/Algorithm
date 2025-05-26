dd06b666eea1f51c9c77ddb4fcb871d008a61d97
Myeongheon Oh <axm0219@naver.com>
2025-05-26 09:45:32 +0900

    [level 3] Title: 단어 변환, Time: 0.38 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,33 +1,30 @@
-from collections import deque
+def can_trans(cur_word, next_word):
+    count = 0
+    for idx, c in enumerate(cur_word):
+        if c == next_word[idx]:
+            count += 1
 
-def solution(begin, target, words):
-    if target not in words:
-        return 0
-    else:
-        return bfs(begin, target, words)
-
-def bfs(begin, target, words):
-    queue = deque()
-    queue.append((begin, 0))
-
-    while queue:
-        now_word, step = queue.popleft()
-
-        if now_word == target:
-            return step
-
-        for word in words:
-            if check(now_word, word):
-                queue.append((word, step+1))
-
-# 한 개의 알파벳만 차이 날때 True 반환
-def check(a, b):
-    cnt = 0
-    for i in range(len(a)):
-        if a[i] != b[i]:
-            cnt += 1
-
-    if cnt == 1:
+    if count == len(cur_word) - 1:
         return True
     else:
         return False
+
+def solution(begin, target, words):
+    answer = float("INF")
+    n = len(words)
+    visited = [False] * n
+
+    def dfs(cur_word, depth):
+        nonlocal answer
+        if cur_word == target:
+            answer = min(answer, depth)
+            return
+
+        for i in range(n):
+            if not visited[i] and can_trans(cur_word, words[i]):
+                visited[i] = True
+                dfs(words[i], depth + 1)
+                visited[i] = False
+
+    dfs(begin, 0)
+    return 0 if answer == float("INF") else answer
