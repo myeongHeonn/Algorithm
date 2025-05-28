f50212708e467543f09863373e6cce6db2d864de
Myeongheon Oh <axm0219@naver.com>
2025-05-28 09:11:09 +0900

    [level 3] Title: 아이템 줍기, Time: 2.77 ms, Memory: 9.42 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
@@ -1,38 +1,49 @@
 from collections import deque
 
 def solution(rectangle, characterX, characterY, itemX, itemY):
-    graph = [[-1 for _ in range(102)] for _ in range(102)]
-    visited = [[0 for _ in range(102)] for _ in range(102)]
-    dx = [0, 1, 0, -1]
-    dy = [-1, 0, 1, 0]
+    MAX = 102
+    board = [[0 for _ in range(MAX)] for _ in range(MAX)]
 
-    # 테두리는 1, 내부는 0
-    for rec in rectangle:
-        x1, y1, x2, y2 = map(lambda x:x*2, rec)
-        for i in range(x1, x2+1):
-            for j in range(y1, y2+1):
-                if x1 < i < x2 and y1 < j < y2:
-                    graph[i][j] = 0
-                elif graph[i][j] != 0:
-                    graph[i][j] = 1
+    for x1, y1, x2, y2 in rectangle:
+        x1 *= 2
+        y1 *= 2
+        x2 *= 2
+        y2 *= 2
+        for i in range(x1, x2 + 1):
+            for j in range(y1, y2 + 1):
+                board[i][j] = 1
 
+    for x1, y1, x2, y2 in rectangle:
+        x1 *= 2
+        y1 *= 2
+        x2 *= 2
+        y2 *= 2
+        for i in range(x1 + 1, x2):
+            for j in range(y1 + 1, y2):
+                board[i][j] = 0
+
+    dy = [-1, 1, 0, 0]
+    dx = [0, 0, -1, 1]
+    visited = [[False for _ in range(MAX)] for _ in range(MAX)]
+    characterX *= 2
+    characterY *= 2
+    itemX *= 2
+    itemY *= 2
+    
     queue = deque()
-    cx, cy, ix, iy = 2*characterX, 2*characterY, 2*itemX, 2*itemY
-    queue.append((cx, cy))
-
+    queue.append((characterX, characterY, 0))
+    
     while queue:
-        x, y = queue.popleft()
-
-        if x == ix and y == iy:
-            return visited[x][y]/2
-
+        x, y, distance = queue.popleft()
+        if x == itemX and y == itemY:
+            return distance // 2
+        
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-
-            if visited[nx][ny] != 0:
-                continue
-
-            if graph[nx][ny] == 1:
-                visited[nx][ny] = visited[x][y] + 1
-                queue.append((nx, ny))
+            
+            if (0 <= nx < MAX and 0 <= ny < MAX and
+                not visited[nx][ny] and
+                board[nx][ny] == 1):
+                queue.append((nx, ny, distance + 1))
+                visited[nx][ny] = True
