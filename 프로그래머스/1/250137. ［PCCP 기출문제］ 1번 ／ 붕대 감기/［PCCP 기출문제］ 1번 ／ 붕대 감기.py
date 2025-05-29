053fc128b336754d73b9f12711fa4588b44166cc
Myeongheon Oh <axm0219@naver.com>
2025-05-29 10:05:06 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.36 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
@@ -1,37 +1,31 @@
-def solution(bandage, health, attacks):
-    answer = 0
-    # 최대 체력
-    max_health = health
-    # 몬스터의 마지막 공격 시간
-    end_time = attacks[-1][0]
-    # 연속 회복 성공 수
-    heal_cnt = 0
-    # 현재 시간
-    current_time = 0
+from collections import deque
 
-    while True: # 시간(time)이 끝나거나 현재체력이 0이 되면 stop
-        # 현재 시간 1초 증가
-        current_time += 1
-        # 현재시간이 몬스터 공격 시간일때
-        if attacks[0][0] == current_time:
-            health -= attacks[0][1]
-            attacks.pop(0)
-            heal_cnt = 0
-            # 체력이 0이 되면 break
+def solution(bandage, health, attacks):
+    MAX = health
+    time = 0
+    sequence = 0
+    attacks = deque(attacks)
+    
+    while attacks:
+        time += 1
+        
+        if attacks and attacks[0][0] == time: # 공격 당할때
+            _, damage = attacks.popleft()
+            health -= damage
+            sequence = 0
+            
             if health <= 0:
-                answer = -1
-                break
-        # 몬스터 공격 시간이 아닐때
-        else:
+                return -1
+            
+            continue
+        else: # 공격 당하지 않을때
+            sequence += 1
             health += bandage[1]
-            heal_cnt += 1
-            if heal_cnt == bandage[0]:
+            if sequence == bandage[0]:
+                sequence = 0
                 health += bandage[2]
-                heal_cnt = 0
-            if health >= max_health:
-                health = max_health
-        # 몬스터의 마지막 공격이 끝나면 그만
-        if current_time == end_time:
-            answer = health
-            break
-    return answer
+            if health > MAX:
+                health = MAX
+                
+            
+    return health
