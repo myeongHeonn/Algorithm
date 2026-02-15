759664af160b645a21d453847123c659bd417448
Myeongheon Oh <axm0219@naver.com>
2026-02-15 18:40:15 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 232 ms, Memory: 21280 KB -BaekjoonHub

---
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int[] dy = { -1, 1, 0, 0 };
+	static int[] dx = { 0, 0, -1, 1 };
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+
+		int test_case = 1;
+
+		while (true) {
+			int n = Integer.parseInt(br.readLine());
+			if (n == 0)
+				break;
+
+			int[][] cave = new int[n][n];
+
+			for (int i = 0; i < n; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < n; j++) {
+					cave[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+
+			int[][] visited = new int[n][n];
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					visited[i][j] = -1;
+				}
+			}
+			
+			visited[0][0] = cave[0][0];
+
+			PriorityQueue<int[]> q = new PriorityQueue<>((q1, q2) -> q1[2] - q2[2]);
+			q.offer(new int[] {0, 0, cave[0][0]});
+
+			while (!q.isEmpty()) {
+				int[] cur = q.poll();
+				int y = cur[0];
+				int x = cur[1];
+
+				if (y == n - 1 && x == n - 1) {
+					sb.append("Problem " + test_case + ": " + visited[y][x]).append("\n");
+					break;
+				}
+
+				for (int d = 0; d < 4; d++) {
+					int ny = y + dy[d];
+					int nx = x + dx[d];
+
+					if (ny < 0 || ny >= n || nx < 0 || nx >= n)
+						continue;
+
+					if (visited[ny][nx] == -1) {
+						visited[ny][nx] = visited[y][x] + cave[ny][nx];
+						q.offer(new int[] {ny, nx, visited[ny][nx]});
+					} else {
+						if (visited[ny][nx] <= visited[y][x] + cave[ny][nx])
+							continue;
+
+						visited[ny][nx] = visited[y][x] + cave[ny][nx];
+						q.offer(new int[] {ny, nx, visited[ny][nx]});
+					}
+
+				}
+			}
+			
+			test_case++;
+		}
+
+		System.out.println(sb);
+	}
+}
