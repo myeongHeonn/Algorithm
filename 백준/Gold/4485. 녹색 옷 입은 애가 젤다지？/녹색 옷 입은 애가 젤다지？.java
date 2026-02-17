aaa97df34cc8802f766f7107cceb26f23300c14f
Myeongheon Oh <axm0219@naver.com>
2026-02-17 15:48:53 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 188 ms, Memory: 21272 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -31,17 +29,17 @@ public class Main {
 				}
 			}
 
-			int[][] visited = new int[n][n];
+			int[][] dist = new int[n][n];
 			for (int i = 0; i < n; i++) {
 				for (int j = 0; j < n; j++) {
-					visited[i][j] = -1;
+					dist[i][j] = Integer.MAX_VALUE;
 				}
 			}
-			
-			visited[0][0] = cave[0][0];
+
+			dist[0][0] = cave[0][0];
 
 			PriorityQueue<int[]> q = new PriorityQueue<>((q1, q2) -> q1[2] - q2[2]);
-			q.offer(new int[] {0, 0, cave[0][0]});
+			q.offer(new int[] { 0, 0, cave[0][0] });
 
 			while (!q.isEmpty()) {
 				int[] cur = q.poll();
@@ -49,7 +47,7 @@ public class Main {
 				int x = cur[1];
 
 				if (y == n - 1 && x == n - 1) {
-					sb.append("Problem " + test_case + ": " + visited[y][x]).append("\n");
+					sb.append("Problem " + test_case + ": " + dist[y][x]).append("\n");
 					break;
 				}
 
@@ -57,23 +55,15 @@ public class Main {
 					int ny = y + dy[d];
 					int nx = x + dx[d];
 
-					if (ny < 0 || ny >= n || nx < 0 || nx >= n)
-						continue;
+					if (ny < 0 || ny >= n || nx < 0 || nx >= n) continue;
 
-					if (visited[ny][nx] == -1) {
-						visited[ny][nx] = visited[y][x] + cave[ny][nx];
-						q.offer(new int[] {ny, nx, visited[ny][nx]});
-					} else {
-						if (visited[ny][nx] <= visited[y][x] + cave[ny][nx])
-							continue;
-
-						visited[ny][nx] = visited[y][x] + cave[ny][nx];
-						q.offer(new int[] {ny, nx, visited[ny][nx]});
+					if (dist[ny][nx] > dist[y][x] + cave[ny][nx]) {
+						dist[ny][nx] = dist[y][x] + cave[ny][nx];
+						q.offer(new int[] { ny, nx, dist[ny][nx] });
 					}
-
 				}
 			}
-			
+
 			test_case++;
 		}
 
