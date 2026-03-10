c3e9ff70f698bbca330a2e73cf37cf404695ccb5
Myeongheon Oh <axm0219@naver.com>
2026-03-11 00:01:00 +0900

    [Silver IV] Title: 동전 0, Time: 100 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		
+		int[] coin = new int[n];
+		
+		for (int i = 0; i < n; i++) {
+			coin[i] = Integer.parseInt(br.readLine());
+		}
+		
+		int count = 0;
+		
+		for (int i = n - 1; i >= 0; i--) {
+			count += k / coin[i];
+			k %= coin[i];
+		}
+		
+		System.out.println(count);
+	}
+}
